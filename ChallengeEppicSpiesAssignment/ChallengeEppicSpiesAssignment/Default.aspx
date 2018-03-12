9b173596d5d3031255b7d5cf48ddb368cf7b4d19
Yohei <yo_kubocchi@hotmail.com>
2018-03-12 02:41:31 -0700

    New Challenge

---
+++ ChallengeEppicSpiesAssignment/ChallengeEppicSpiesAssignment/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEppicSpiesAssignment.Default" MaintainScrollPositionOnPostback="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 151px;
+            height: 190px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="" class="auto-style1" src="epic-spies-logo.jpg" /><br />
+            <h2 style="font-family: Arial, Helvetica, sans-serif">Spy New Assignment Form</h2>
+            <br />
+            Spy Code Name: <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name:
+            <asp:TextBox ID="assignmentTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Precious Assingment:<asp:Calendar ID="previousCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Start Date of New Assingment:<asp:Calendar ID="startCalendar" runat="server" OnSelectionChanged="startCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            Projected End Date of New Assignment:<asp:Calendar ID="endCalendar" runat="server" OnSelectionChanged="endCalendar_SelectionChanged"></asp:Calendar>
+            Assignment period:
+            <asp:Label ID="periodLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Assign Spy" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEppicSpiesAssignment/ChallengeEppicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEppicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                previousCalendar.SelectedDate = DateTime.Today;
+                startCalendar.SelectedDate = DateTime.Today.AddDays(14);
+                endCalendar.SelectedDate = DateTime.Today.AddDays(21);
+            }
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            Boolean vali = true;
+            string error = "";
+            string name = nameTextBox.Text;
+            string assignment = assignmentTextBox.Text;
+            DateTime previousAssEnd = previousCalendar.SelectedDate;
+            DateTime startAss = startCalendar.SelectedDate;
+            DateTime endAss = endCalendar.SelectedDate;
+
+            TimeSpan preAssPeriod = startAss.Subtract(previousAssEnd);
+            TimeSpan assPeriod = endAss.Subtract(startAss);
+
+            double budget = assPeriod.Days * 500; // $500/day
+            if (assPeriod.Days > 21) budget += 1000;
+
+            string result = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C2}",
+                                name, assignment, budget);
+
+            // validate
+            // Checking if Code Name or New Assignment Name is empty
+            if (name == "" || assignment == "")
+            {
+                vali = false;
+                error += "Error: Must input Spy Code Name and New Assignment Name.<br />";
+            }
+            // Checking if it allows more than two weeks between two assignments
+            if (preAssPeriod.Days < 14)
+            {
+                vali = false;
+                error += "Error: Must allow at least two weeks between previous assignment and new assignment.<br />";
+            }
+            // Checking if End Date of Assignment comes after Start Date
+            if (assPeriod.Days < 1)
+            {
+                vali = false;
+                error += "Error: End Date must comes afte Start date.";
+            }
+            // End validate
+
+            resultLabel.Text = (vali) ? result : error;
+                
+        }
+
+        protected void startCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            DateTime startAss = startCalendar.SelectedDate;
+            DateTime endAss = endCalendar.SelectedDate;
+            TimeSpan assPeriod = endAss.Subtract(startAss);
+            periodLabel.Text = assPeriod.Days.ToString();
+        }
+
+        protected void endCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            DateTime startAss = startCalendar.SelectedDate;
+            DateTime endAss = endCalendar.SelectedDate;
+            TimeSpan assPeriod = endAss.Subtract(startAss);
+            periodLabel.Text = assPeriod.Days.ToString();
+        }
+    }
+}
